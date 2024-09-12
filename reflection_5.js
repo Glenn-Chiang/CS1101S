bcdaafb98950d71570627d5c63e6c9e34c293d46
Glenn <122365492+Glenn-Chiang@users.noreply.github.com>
2024-09-12 10:36:56 +0800

    Update reflection_5.js

--- reflection_5.js
+++ reflection_5.js
@@ -1,6 +1,15 @@
 function flatten_list(lol) {
-    return accumulate((lst, res) => append(lst, res), 
-                      null, lol);
+    return accumulate(append, null, lol);
+}
+
+function flatten_tree(tree) {
+    return accumulate(
+        (subtree, res) => 
+            is_list(subtree) 
+                ? append(flatten_tree(subtree), res)
+                : append(list(subtree), res),
+        null,
+        tree);
 }
 
 function tree_sum(tree) {
@@ -12,10 +21,6 @@ function tree_sum(tree) {
           + tree_sum(tail(tree));
 }
 
-function tree_sum(tree) {
-    return accumulate_tree(x => x, (x, y) => x + y, 0, tree);
-}
-
 function accumulate_tree(f, op, initial, tree) {
     return accumulate(
         (subtree, res) => 
@@ -23,6 +28,15 @@ function accumulate_tree(f, op, initial, tree) {
                 ? accumulate_tree(f, op, res, subtree)
                 : op(f(subtree), res),
         initial, tree);
+}
+// The binary function op must be commutative and associative
+
+function tree_sum(tree) {
+    return accumulate_tree(x => x, (x, y) => x + y, 0, tree);
+}
+
+function flatten_tree(tree) {
+    return accumulate_tree(list, append, null, tree);
 }
 
 const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
