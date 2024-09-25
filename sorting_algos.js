f26f3c1496dc6c1bed60b6d24b5f75a15c3debeb
Glenn <122365492+Glenn-Chiang@users.noreply.github.com>
2024-09-25 11:49:50 +0800

    Update sorting_algos.js
    Add time complexity

--- sorting_algos.js
+++ sorting_algos.js
@@ -1,29 +1,29 @@
 function insertion_sort(xs) {
+    function insert(x, xs) {
+        return is_null(xs)
+            ? list(x)
+            : x <= head(xs)
+            ? pair(x, xs)
+            : pair(head(xs), insert(x, tail(xs)));
+    }
     return is_null(xs)
         ? null
         : insert(head(xs), insertion_sort(tail(xs)));
 }
-
-function insert(x, xs) {
-    return is_null(xs)
-        ? list(x)
-        : x <= head(xs)
-        ? pair(x, xs)
-        : pair(head(xs), insert(x, tail(xs)));
-}
+// Best: N, Worst: N^2, Average: N^2
 
 function selection_sort(xs) {
+    function select(xs) {
+        return accumulate((x, y) => x < y ? x : y, head(xs), tail(xs));
+    }
     if (is_null(xs)) {
         return xs;
     } else {
-        const x = smallest(xs);
+        const x = select(xs);
         return pair(x, selection_sort(remove(x, xs)));
     }
 }
-
-function smallest(xs) {
-    return accumulate((x, y) => x < y ? x : y, head(xs), tail(xs));
-}
+// Best: N^2, Worst: N^2, Average: N^2
 
 function merge_sort(xs) {
     if (is_null(xs) || is_null(tail(xs))) {
@@ -36,6 +36,7 @@ function merge_sort(xs) {
                      merge_sort(second_half));
     }
 }
+// Best: NlogN, Worst: NlogN, Average: NlogN
 
 function merge(xs, ys) {
     if (is_null(xs)) {
